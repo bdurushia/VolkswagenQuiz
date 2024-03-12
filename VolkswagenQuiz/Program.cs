c3708a883d58047fed41fa790a8fb908e5d7ddf0
bdurushia <b.durushia@gmail.com>
2024-03-12 09:32:43 -0500

    initial commit

---
+++ VolkswagenQuiz/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace VolkwagenQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Volkswagen Quiz!\nPress Enter to Start!");
+            Console.ReadLine();
+
+            var question1 = new Question()
+            {
+                QuestionText = "Question 1: Who designed the VW Beetle?\n"
+            };
+
+            question1.Answers.Add(("A. Ferdinand Porsche", true));
+            question1.Answers.Add(("B. Josef Ganz", false));
+            question1.Answers.Add(("C. Heinrich Stein", false));
+            question1.Answers.Add(("D. Adolf Hitler", false));
+
+            var correctAnswer = question1.Answers[0];
+
+            // Console.WriteLine(correctAnswer.Item1);
+            // Console.WriteLine(correctAnswer.Item2);
+
+            // ToDo - Find Correct Answer - Loop?
+
+            question1.PrintQuestion();
+            //question1.PrintAnswer(correctAnswer);
+
+            var question2 = new Question() {QuestionText = "Question 2"};
+
+            //question2.Answers.Add("Q2 Answer");
+
+            //Console.WriteLine(question2.Answers[0]);
+        }
+    }
+}
---
+++ VolkswagenQuiz/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolkwagenQuiz
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        
+        // Tuples - Two things that are bound together
+        public List<(string, bool)> Answers { get; set; } = new List<(string, bool)>();
+
+        public void PrintQuestion()
+        {
+            Console.WriteLine(QuestionText);
+            Console.WriteLine($"{Answers[0]}\n{Answers[1]}\n{Answers[2]}\n{Answers[3]}\n");
+        }
+
+        public void PrintAnswer(string correctAnswer)
+        {
+            var userAnswer = Console.ReadLine().ToLower();
+            Console.WriteLine(userAnswer == correctAnswer ? "Correct!" : "Incorrect");
+        }
+    }
+}
